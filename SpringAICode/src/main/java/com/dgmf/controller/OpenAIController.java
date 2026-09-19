004c9aea3fce08c51ec080617ec9fcf9ab5b2b43
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-09-19 11:24:57 +0200

    Working with ChatClient

--- SpringAICode/src/main/java/com/dgmf/controller/OpenAIController.java
+++ SpringAICode/src/main/java/com/dgmf/controller/OpenAIController.java
@@ -1,5 +1,6 @@
 package com.dgmf.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,16 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenAIController {
-    private final OpenAiChatModel chatModel;
+    // private final OpenAiChatModel chatModel;
+    private final ChatClient chatClient;
 
     public OpenAIController(OpenAiChatModel chatModel) {
-        this.chatModel = chatModel;
+        // this.chatModel = chatModel;
+        this.chatClient = ChatClient.create(chatModel);
     }
 
     @GetMapping("/api/{message}")
     public String getAnswer(@PathVariable String message) {
-        String response = chatModel.call(message);
+        // String response = chatModel.call(message);
+        String response = chatClient
+                .prompt(message)
+                .call()
+                .content();
 
-        return response;
+        // return response;
+        return "With ChatClient : " + response;
     }
 }
