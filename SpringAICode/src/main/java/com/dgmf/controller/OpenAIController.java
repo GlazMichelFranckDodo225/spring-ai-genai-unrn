c984241694b86381a09753c8f610bd58af6cae70
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-09-19 11:32:20 +0200

    Working with ChatClient using ResponseEntity

--- SpringAICode/src/main/java/com/dgmf/controller/OpenAIController.java
+++ SpringAICode/src/main/java/com/dgmf/controller/OpenAIController.java
@@ -2,6 +2,8 @@ package com.dgmf.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,8 @@ public class OpenAIController {
     }
 
     @GetMapping("/api/{message}")
-    public String getAnswer(@PathVariable String message) {
+    // public String getAnswer(@PathVariable String message) {
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
         // String response = chatModel.call(message);
         String response = chatClient
                 .prompt(message)
@@ -25,6 +28,7 @@ public class OpenAIController {
                 .content();
 
         // return response;
-        return "With ChatClient : " + response;
+        // return "With ChatClient : " + response;
+        return ResponseEntity.ok(response);
     }
 }
